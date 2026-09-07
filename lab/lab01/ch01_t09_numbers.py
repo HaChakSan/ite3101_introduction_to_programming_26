a74e67b40e1baa53994828cd8af2092ef9957268
HaChakSan <isopjason@gmail.com>
2026-09-07 07:11:41 +0000

    [skip actions] lab/lab01/ch01_t09_numbers.py

--- lab/lab01/ch01_t09_numbers.py
+++ lab/lab01/ch01_t09_numbers.py
@@ -4,5 +4,6 @@ num_people = 6
 whole_cucumbers_per_person = cucumbers // num_people
 print(whole_cucumbers_per_person)
 
-float_cucumbers_per_person = cucumbers / n
+float_cucumbers_per_person = cucumbers / num_people
+print()
 
