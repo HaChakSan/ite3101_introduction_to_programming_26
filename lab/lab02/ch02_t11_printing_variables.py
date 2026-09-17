2d1486be3b282fc513cb11d3df90dfe7cf4df1a8
HaChakSan <isopjason@gmail.com>
2026-09-17 08:30:04 +0000

    lab/lab02/ch02_t11_printing_variables.py

--- lab/lab02/ch02_t11_printing_variables.py
+++ lab/lab02/ch02_t11_printing_variables.py
@@ -1,3 +1,7 @@
 """Assign the string "Ping!" to
 the variable the_machine_goes on
 line 5, then print it out on line 6!"""
+# Assuming this starts from line 1:
+# Line 1-4: (blank or other code)
+the_machine_goes = "Ping!"  # Line 5
+print(the_machine_goes)     # Line 6
