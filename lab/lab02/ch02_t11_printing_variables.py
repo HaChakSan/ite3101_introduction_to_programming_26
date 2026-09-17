5e497d0809217890517c88849e166d8c43f7e4d7
HaChakSan <isopjason@gmail.com>
2026-09-17 08:32:09 +0000

    [skip actions] lab/lab02/ch02_t12_string_concatenation.py

--- lab/lab02/ch02_t11_printing_variables.py
+++ lab/lab02/ch02_t11_printing_variables.py
@@ -1,7 +1,6 @@
 """Assign the string "Ping!" to
 the variable the_machine_goes on
 line 5, then print it out on line 6!"""
-# Assuming this starts from line 1:
-# Line 1-4: (blank or other code)
-the_machine_goes = "Ping!"  # Line 5
-print(the_machine_goes)     # Line 6
+
+the_machine_goes = "Ping!"  
+print(the_machine_goes)    
