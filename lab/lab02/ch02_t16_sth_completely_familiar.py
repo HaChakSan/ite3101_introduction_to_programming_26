a8a1b6bdc53a7eb5a32b9d5e9d39e14a550f8a94
HaChakSan <isopjason@gmail.com>
2026-09-17 08:53:24 +0000

    lab/lab02/ch02_t16_sth_completely_familiar.py

--- lab/lab02/ch02_t16_sth_completely_familiar.py
+++ lab/lab02/ch02_t16_sth_completely_familiar.py
@@ -1,1 +1,5 @@
 # Write your code below, starting on line 3!
+
+my_string = "Hello, World!"  # Line 3: Create your string variable
+print(len(my_string))        # Line 4: Print the length of the string
+print(my_string.upper())     # Line 5: Print the uppercase version
