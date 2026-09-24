ef68fe706afaca50b454da2822b2f454f21c3201
HaChakSan <isopjason@gmail.com>
2026-09-24 08:05:29 +0000

    [skip actions] lab/lab03/ch03_t05_pretty_time.py

--- lab/lab03/ch03_t05_pretty_time.py
+++ lab/lab03/ch03_t05_pretty_time.py
@@ -2,10 +2,9 @@
 from datetime import datetime
 
 now = datetime.now()
-from datetime import datetime
 
 now = datetime.now()
 
-# Print the time formatted as hh:mm:ss
+
 print('%02d:%02d:%02d' % (now.hour, now.minute, now.second))
 print('%02d:%02d:%02d' % (now.hour, now.minute, now.second))
