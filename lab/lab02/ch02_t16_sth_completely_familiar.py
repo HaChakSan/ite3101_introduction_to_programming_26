ccfd32b3f1a6a9efa9759e9b93b0d51f6959659a
HaChakSan <isopjason@gmail.com>
2026-09-24 07:20:22 +0000

    lab/lab03/ch03_t01_the_datetime_library.py

--- lab/lab02/ch02_t16_sth_completely_familiar.py
+++ lab/lab02/ch02_t16_sth_completely_familiar.py
@@ -1,5 +1,5 @@
 # Write your code below, starting on line 3!
 
-my_string = "Hello, World!"  # Line 3: Create your string variable
-print(len(my_string))        # Line 4: Print the length of the string
-print(my_string.upper())     # Line 5: Print the uppercase version
+my_string = "Hello, World!"  
+print(len(my_string))        
+print(my_string.upper())  
