3acb2a268dfc0a011130949a96e9f7d28e32feee
HaChakSan <isopjason@gmail.com>
2026-09-24 08:12:54 +0000

    [skip actions] lab/lab03/ch03_t05_pretty_time.py

--- lab/lab03/ch03_t05_pretty_time.py
+++ lab/lab03/ch03_t05_pretty_time.py
@@ -5,5 +5,5 @@ now = datetime.now()
 
 now = datetime.now()
 
-print('%02d:%02d:%02d' % (now.month, now.day, now.yearnow.hour, now.minute, now.second))
+print('%02d:%02d:%02d' % (now.month, now.day, now.year , now.hour, now.minute, now.second))
 
