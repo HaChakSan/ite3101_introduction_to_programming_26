11866bdecdd04dac8d67c735e32f28e117635ebc
HaChakSan <isopjason@gmail.com>
2026-09-24 07:31:06 +0000

    [skip actions] lab/lab03/ch03_t03_extracting_Info.py

--- lab/lab03/ch03_t03_extracting_Info.py
+++ lab/lab03/ch03_t03_extracting_Info.py
@@ -5,3 +5,11 @@ now = datetime.now()
 print(now.year)
 print(now.mouth)
 print(now.day)
+from datetime import datetime
+
+now = datetime.now()
+
+# Print the individual components of the date
+print(now.year)
+print(now.month)
+print(now.day)
